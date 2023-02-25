4201b7f38d3080bb714cc28e9de297303de12f48
Eggris <116574709+Eggris@users.noreply.github.com>
2023-02-25 19:02:41 +0000

    Add files via upload

--- Client.java
+++ Client.java
@@ -14,7 +14,7 @@ public class Client implements Runnable {
     @Override
     public void run() {
         try {
-            Socket client = new Socket("127.0.0.1", 9999);
+            client = new Socket("127.0.0.1", 9999);
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
@@ -55,6 +55,7 @@ public class Client implements Runnable {
                 while (!done) {
                     String message = inReader.readLine();
                     if (message.equals("/quit")) {
+                        out.println(message);
                         inReader.close();
                         shutdown();
                     } else {
